63cb95bd36123a12f276a92c10afb6d7934328c0
Jeff <>
2022-10-25 20:01:10 +0800

    vercel

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <span className="icn-logo"><i className="material-icons">code</i></span>
+    <span className="icn-logo">Jeff Tabios<i className="material-icons">code</i></span>
     <ul className="main-nav">
       <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink to="/about">About</NavLink></li>
